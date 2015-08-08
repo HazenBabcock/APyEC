19be9e371cd3d9327a40d97e3a8fb94a253a37cc
Hazen Babcock <hbabcock@mac.com>
2015-08-08 08:13:09 -0400

    Save and restore geometry and settings.

--- jezen/editor.py
+++ jezen/editor.py
@@ -5,6 +5,7 @@
 """
 
 import markdown
+import os
 
 from PyQt4 import QtCore, QtGui, QtWebKit
 
@@ -23,26 +24,38 @@ class Editor(QtGui.QDialog):
     def __init__(self, note, parent = None):
         QtGui.QDialog.__init__(self, parent)
         self.note = note
-        self.update_timer = QtCore.QTimer()
+        self.settings = QtCore.QSettings("jezen", "jezen")
 
+        self.attach_directory = str(self.settings.value("attach_directory", ".").toString())
+        
+        # Note editting timer, to reduce the number of update
+        # events when the user is typing.
+        self.update_timer = QtCore.QTimer()
         self.update_timer.setInterval(200)
         self.update_timer.setSingleShot(True)
         
         self.ui = editorUi.Ui_Dialog()
         self.ui.setupUi(self)
 
-        self.ui.attachmentsMVC.newNote(self.note)
-        
+        # This lets the edit window go behind the main window.
         self.setWindowFlags(QtCore.Qt.Window)
         
+        self.ui.attachmentsMVC.newNote(self.note)        
         self.ui.noteTextEdit.setText(self.note.getMarkdown())
-                
+
+        # Restore Geometry.
+        self.restoreGeometry(self.settings.value("edit_dialog").toByteArray())
+        self.ui.editSplitter.restoreState(self.settings.value("edit_splitter").toByteArray())
+        self.ui.keywordSplitter.restoreState(self.settings.value("keyword_splitter").toByteArray())
+        self.ui.viewEditSplitter.restoreState(self.settings.value("view_edit_splitter").toByteArray())
+        
         self.viewer = Viewer(self.ui.noteGroupBox)
         layout = QtGui.QHBoxLayout()
         layout.addWidget(self.viewer)
         self.ui.noteGroupBox.setLayout(layout)
         self.viewer.newNoteEdit(note)
 
+        # Connect signals.
         self.ui.attachUploadButton.clicked.connect(self.handleAttachUpload)
         self.ui.closeButton.clicked.connect(self.close)
         self.ui.noteTextEdit.textChanged.connect(self.handleTextChanged)
@@ -50,17 +63,25 @@ class Editor(QtGui.QDialog):
         self.update_timer.timeout.connect(self.handleUpdateTimer)
 
         self.handleUpdateTimer()
-        
+
+    def closeEvent(self, event):
+        self.settings.setValue("edit_dialog", self.saveGeometry())
+        self.settings.setValue("edit_splitter", self.ui.editSplitter.saveState())
+        self.settings.setValue("keyword_splitter", self.ui.keywordSplitter.saveState())
+        self.settings.setValue("view_edit_splitter", self.ui.viewEditSplitter.saveState())
+
     def getMarkdown(self):
         return str(self.ui.noteTextEdit.toPlainText())
 
     def handleAttachUpload(self, boolean):
         upload_filename = QtGui.QFileDialog.getOpenFileName(self,
                                                             "Upload File",
-                                                            ".",
+                                                            self.attach_directory,
                                                             "*")
         if upload_filename:
-            self.ui.attachmentsMVC.addAttachment(str(upload_filename))
+            upload_filename = str(upload_filename)
+            self.attach_directory = os.path.dirname(upload_filename)
+            self.ui.attachmentsMVC.addAttachment(upload_filename)
         
     # FIXME: Need to check if the note has changed to reduce spurious commits?
     def handleSave(self):
